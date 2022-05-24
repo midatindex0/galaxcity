a2b075ce8802ecb7d78d8b38aa6ecdba909ebccc
ayush-py-dev <clashofserver8823@gmail.com>
2022-05-24 20:22:29 +0530

    controls.py and req.txt

--- src/cogs/controls.py
+++ src/cogs/controls.py
@@ -15,12 +15,12 @@ def circle(pfp):
     pfp.putalpha(mask)
     return pfp
 
-def get_profile(self,member:discord.Member,level,fails,hint_used):
+def get_profile(self,member:discord.Member,level,fails,score,hint_used):
         pfp=circle(member.avatar)
         color="#ffffff"
         bg=Image.open("asset/img/bg.png")
         bg.paste(pfp,(17,67),pfp)
-        font=ImageFont.truetype("src/asset/fonts/font.ttf",104)
+        font=ImageFont.truetype("asset/fonts/font.ttf",104)
         # 330,135
         draw=ImageDraw.Draw(bg)
         draw.text(
@@ -30,8 +30,8 @@ def get_profile(self,member:discord.Member,level,fails,hint_used):
             fill=color
             )
         # 48,392
-        name=input("Enter Name: ")
-        font2=ImageFont.truetype("src/asset/fonts/font.ttf",62)
+        name=str(member)
+        font2=ImageFont.truetype("asset/fonts/font.ttf",62)
         if len(name)>16:
             name=f"{name[0:16]}..."
         draw.text(
@@ -41,35 +41,37 @@ def get_profile(self,member:discord.Member,level,fails,hint_used):
             fill=color
         )
 
-        font3=ImageFont.truetype("src/asset/fonts/font.ttf",105)
+        font3=ImageFont.truetype("asset/fonts/font.ttf",105)
         # 90,585
         draw.text(
             xy=(90,585),
-            text=f"Level: {input('Enter Level: ')}",
+            text=str(level),
             font=font3,
             fill=color
         )
 
         draw.text(
             xy=(90,735),
-            text=f"Fails: {input('Enter Fail: ')}",
+            text=str(fails),
             font=font3,
             fill=color
         )
 
         draw.text(
             xy=(90,885),
-            text=f"Tries: {input('Enter Tries: ')}",
+            text=str(hint_used),
             font=font3,
             fill=color
         )
 
         draw.text(
             xy=(90,1035),
-            text=f"Hints: {input('Enter Hints: ')}",
+            text=str(score),
             font=font3,
             fill=color
         )
+
+        return bg
 
 class Controls(commands.Cog):
     """
