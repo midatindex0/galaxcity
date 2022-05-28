ca777c7b3e7d016d8f2aeb7f779cfafbd3c25308
midnightFirefly <83584402+lonely-code-cube@users.noreply.github.com>
2022-05-28 08:09:42 +0530

    Delete config.py

--- src/config/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BOT_LOG_LEVEL = 25
-LOG_LEVEL = 25
-
-_MONGO_USER = "lonely-code-cube" # Mongodb username
-
-_MONGO_PASS = "GVvHJLfyFodJyjCo" # Personal mongo pass
-_MONGO_URI = f"mongodb+srv://{_MONGO_USER}:{_MONGO_PASS}@cluster0.7zvhe.mongodb.net/Main?retryWrites=true&w=majority"
